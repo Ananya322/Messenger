b89ba4acfe97e78ea4bbfd053bbb3203abbd1b96
Ananya <ananyasinghal0022@gmail.com>
2022-06-04 20:36:44 +0530

    Initial

---
+++ main.py
@@ -0,0 +1,5 @@
+import pywhatkit as whatsapp
+whatsapp.sendwhatmsg("+919840049485", "Hi, I'm Ananya from Robovitics-the official robotics club of VIT.I believe you've heard of tech week in VIT. "
+                                      "Being a robotics club we bring to you Machine Design with Fusion 360 - sketching, 3D modelling and design presentation with advanced industrial techniques. On 23rd, 24th and 25th of November this year. "
+                                      "We believe Machine design is an invaluable skill that every core internship, J component and hack demands from a mechanical student."
+                                      "Register for this Event on Vtop to enhance your skills. This can become your golden chance to get an edge over other students.", 17, 8)
